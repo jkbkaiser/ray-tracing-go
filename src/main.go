1b532aee6e90a89173cbce79732f886254a814e2
Jakob Kaiser <jakob.j.jeep@gmail.com>
2024-11-07 19:14:04 +0100

    completed chapter 14

--- src/main.go
+++ src/main.go
@@ -5,36 +5,66 @@ import (
 	"jkbkaiser/ray-tracing-go/pkg/color"
 	"jkbkaiser/ray-tracing-go/pkg/hittable"
 	"jkbkaiser/ray-tracing-go/pkg/material"
+	"jkbkaiser/ray-tracing-go/pkg/util"
 	"jkbkaiser/ray-tracing-go/pkg/vec3"
+	"math/rand/v2"
 )
 
 func main() {
-	mat_ground := material.Lambertian{Albedo: color.Color{R: .8, G: .8, B: .0}}
-	mat_center := material.Lambertian{Albedo: color.Color{R: .1, G: .2, B: .5}}
-	mat_left := material.Dialectric{RefractionIndex: 1.5}
-	mat_bubble := material.Dialectric{RefractionIndex: 1. / 1.5}
-	mat_right := material.Metal{Albedo: color.Color{R: .8, G: .6, B: .2}, Fuzz: 1.}
-
 	world := hittable.HittableList{}
-	world.Add(hittable.NewSphere(vec3.Vec3{X: 0., Y: -100.5, Z: -1.}, 100., mat_ground))
-	world.Add(hittable.NewSphere(vec3.Vec3{X: 0., Y: 0., Z: -1.2}, .5, mat_center))
-	world.Add(hittable.NewSphere(vec3.Vec3{X: -1., Y: 0., Z: -1.}, .5, mat_left))
-	world.Add(hittable.NewSphere(vec3.Vec3{X: -1., Y: 0., Z: -1.}, .4, mat_bubble))
-	world.Add(hittable.NewSphere(vec3.Vec3{X: 1., Y: 0., Z: -1.}, .5, mat_right))
+
+	ground_mat := material.Lambertian{Albedo: color.Color{R: .5, G: .5, B: .5}}
+	world.Add(hittable.NewSphere(vec3.Vec3{X: 0., Y: -1000., Z: 0.}, 1000., ground_mat))
+
+	for a := -11; a < 11; a++ {
+		for b := -11; b < 11; b++ {
+			chooseMat := rand.Float64()
+			center := vec3.Vec3{
+				X: float64(a) + .9*rand.Float64(),
+				Y: .2,
+				Z: float64(b) + .9*rand.Float64(),
+			}
+
+			if center.Subtract(vec3.Vec3{X: 4., Y: .2, Z: 0.}).Length() > .9 {
+				if chooseMat < 0.8 {
+					albedo := color.FromVec(vec3.Random()).Mult(color.FromVec(vec3.Random()))
+					mat := material.Lambertian{Albedo: albedo}
+					world.Add(hittable.NewSphere(center, 0.2, mat))
+				} else if chooseMat < 0.95 {
+					albedo := color.FromVec(vec3.RandomRange(0.5, 1))
+					fuzz := util.RandomFloat(0, 0.5)
+					mat := material.Metal{Albedo: albedo, Fuzz: fuzz}
+					world.Add(hittable.NewSphere(center, 0.2, mat))
+				} else {
+					mat := material.Dialectric{RefractionIndex: 1.5}
+					world.Add(hittable.NewSphere(center, 0.2, mat))
+				}
+			}
+		}
+	}
+
+	mat1 := material.Dialectric{RefractionIndex: 1.5}
+	world.Add(hittable.NewSphere(vec3.Vec3{X: 0., Y: 1., Z: 0.}, 1., mat1))
+
+	mat2 := material.Lambertian{Albedo: color.Color{R: .4, G: .2, B: .1}}
+	world.Add(hittable.NewSphere(vec3.Vec3{X: -4., Y: 1., Z: 0.}, 1., mat2))
+
+	mat3 := material.Metal{Albedo: color.Color{R: .7, G: .6, B: .5}, Fuzz: 0.}
+	world.Add(hittable.NewSphere(vec3.Vec3{X: 4., Y: 1., Z: 0.}, 1., mat3))
 
 	cam := camera.New()
 	cam.AspectRatio = 16. / 9.
 	cam.ImageWidth = 400
-	cam.NumberOfSamples = 100
+	cam.NumberOfSamples = 10
 	cam.MaxDepth = 50
 
 	cam.FieldOfView = 20
-	cam.LookFrom = vec3.Vec3{X: -2, Y: 2, Z: 1}
-	cam.LookAt = vec3.Vec3{X: 0, Y: 0, Z: -1}
+	cam.LookFrom = vec3.Vec3{X: 13, Y: 2, Z: 3}
+	cam.LookAt = vec3.Vec3{X: 0, Y: 0, Z: 0}
 	cam.VUp = vec3.Vec3{X: 0, Y: 1, Z: 0}
 
-	cam.DefocusAngle = 10.
-	cam.FocusDist = 3.4
+	cam.DefocusAngle = .6
+	cam.FocusDist = 10.
 
 	cam.Initialize()
 	cam.Render(world)
